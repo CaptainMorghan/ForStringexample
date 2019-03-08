4854855472e5683def14e9037ef9d85ec38700c9
CaptainMorghan <47458916+CaptainMorghan@users.noreply.github.com>
2019-03-08 15:06:44 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ForString_example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string myWord = "It's only a flesh wound!";
+            string output = "";
+
+            for (int i = 0; i < myWord.Length; i++) 
+            {
+                output += myWord[i].ToString() +
+                    Environment.NewLine;
+            }
+            lblOutput.Content = output +
+                Environment.NewLine + myWord;
+        }
+    }
+}
